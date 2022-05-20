d90d5fad6880179d51d2fa05d5bf3016e4e6f55e
alequinterok <aquintero@fi.uba.ar>
2022-05-20 09:47:45 -0300

    agregado de pre y post condiciones

--- escritores.h
+++ escritores.h
@@ -15,19 +15,45 @@ private:
     int anio_nacimiento;
     int anio_fallecimiento;
 
-public:
+public: 
+    //PRE: -
+    //POST: Crea un escritor
     Escritor();
-
+    
+    //PRE: El parametro pasado debe ser de tipo string
+    //POST: Define el nombre y apellido del escritor
     void definir_nombre_apellido(string);
+    
+    //PRE: El parametro pasado debe ser de tipo string
+    //POST: Define la nacionalidad del escritor
     void definir_nacionalidad(string);
+
+    //PRE: El parametro pasado debe ser de tipo int
+    //POST: Define el año de nacimiento del escritor
     void definir_anio_nacimiento(int);
+    
+    //PRE: El parametro pasado debe ser de tipo int
+    //POST: Define el año de fallecimiento del escritor
     void definir_anio_fallecimiento(int);
 
+    //PRE: El parametro pasado debe ser de tipo int
+    //POST: Modifica el valor anio_fallecimiento por el pasado como parametro
     void modificar_anio_fallecimiento(int);
     
+    //PRE: -
+    //POST: Devuelve el nombre y apellido del escritor
     string obtener_nombre_apellido();
+    
+    //PRE: -
+    //POST: Devuelve la nacionalidad del escritor
     string obtener_nacionalidad();
+    
+    //PRE: -
+    //POST: Devuelve el año de nacimiento del escritor
     int obtener_anio_nacimiento();
+    
+    //PRE: -
+    //POST: Devuelve el año de fallecimiento del escritor
     int obtener_anio_fallecimiento();
 
 };
